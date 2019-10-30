d27a7e8c0fa4c4a1723972b7455a17ff519a58c8
victorgois <victorgois18@gmail.com>
2019-10-30 17:01:54 -0300

    modularizando código

--- trendTweets.py
+++ trendTweets.py
@@ -20,35 +20,43 @@ auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
 
 # Create the api to connect to twitter with your creadentials
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True, compression=True)
-#---------------------------------------------------------------------------------------------------------------------
-# wait_on_rate_limit= True;  will make the api to automatically wait for rate limits to replenish
-# wait_on_rate_limit_notify= Ture;  will make the api  to print a notification when Tweepyis waiting for rate limits to replenish
-#---------------------------------------------------------------------------------------------------------------------
 
-
-#---------------------------------------------------------------------------------------------------------------------
-# The following loop will print most recent statuses, including retweets, posted by the authenticating user and that user’s friends. 
-# This is the equivalent of /timeline/home on the Web.
-#---------------------------------------------------------------------------------------------------------------------
-
-#for status in tweepy.Cursor(api.home_timeline).items(200):
-#	print(status._json)
-	
-#---------------------------------------------------------------------------------------------------------------------
-# Twitter API development use pagination for Iterating through timelines, user lists, direct messages, etc. 
-# To help make pagination easier and Tweepy has the Cursor object.
-sfo_trends = api.trends_place(id =23424768)
-
-#print(json.dumps(sfo_trends, indent=4))
-
-data =  json.loads(open('./trendingTweets-28-10.json').read())
 hashtagsArray = []
+tweetsArray = []
 
-for element in data[0]['trends']:
-    hashtagsArray.append(element['name'])
+#Script funcions below
 
-for element in hashtagsArray:
-    tweets = tweepy.Cursor(api.search, q=element, count=10)
-    for item in tweets.items(10):
-        print(json.dumps(item._json))
+def getTrends(api):
+    # Open a file called trends.json and parsing to insert the trending info
+    with open('trends.json','w') as f:
+        trendsBrasil = api.trends_place(id =23424768)
+        json.dump(trendsBrasil,f,indent=4)
+    return trendsBrasil
 
+def getHashtags(trendsBrasil,hashtagsArray):
+    # Populate the hashtagsArray with the 'name' field of the trend info
+    for element in trendsBrasil[0]['trends']:
+        hashtagsArray.append(element['name'])
+    return hashtagsArray
+
+def getTweets(hashtagsArray):
+    # Open a file called tweets.json, parsing, and for each element of the hashtagsArray do the research
+    with open('tweets.json','w') as f:
+        for element in hashtagsArray:
+            tweets = tweepy.Cursor(api.search, q=element, count=10)
+    # Doing two copies of the tweets info. One copy for json file and one for variable array inside the script
+            for item in tweets.items(10):
+                tweetsArray.append((json.dumps(item._json)))
+                json.dump(item._json, f, indent=4)
+    return tweetsArray
+
+def parseUrls(tweets):
+    
+    for entities in tweets:
+        print(entities)
+
+trends = getTrends(api)
+hashtags = getHashtags(trends, hashtagsArray)
+tweets = getTweets(hashtags)
+
+parseUrls(tweets)
