c898cfc00a970e78c269e171922e88fc9ed2c73e
Victor Góis <victorgois18@gmail.com>
2020-03-02 12:55:56 -0300

    *

--- teste.py
+++ teste.py
@@ -1,6 +1,18 @@
+import json
+import tweepy
 import time
+from collections import Counter 
 
-timestr = time.strftime("%Y%m%d-%H%M%S")
+#timestr = time.strftime("%Y%m%d-%H")
+timestr = '20200208-10'
 
-with open('./testes/'+ timestr + '.txt', 'w', encoding='utf-8') as parsed:
-    parsed.write(timestr)
+def parse():
+    # Count method - ref: https://www.w3schools.com/python/ref_list_count.asp
+      with open('./parsedData/mentions/'+ timestr + '.json', 'w', encoding='utf-8') as parsed:
+        print(parsed)
+"""         for item in parsed:
+            text = json.loads(item)
+            text.replace("'",'"')
+            print(text) """
+
+parse()
