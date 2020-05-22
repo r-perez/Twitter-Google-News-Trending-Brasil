9c520977f0c5a0c30351135135ccc63b36dae72c
Victor Góis <victorgois18@gmail.com>
2020-05-22 20:29:15 -0300

    *

--- teste.py
+++ teste.py
@@ -7,6 +7,7 @@ from collections import Counter
 timestr = '20200208-10'
 array = []
 newarray = []
+sub_dict = {}
 
 def parse():
       with open('./parsedData/mentions/20200509-20' + '.json', 'r', encoding='utf-8') as parsed:
@@ -16,14 +17,21 @@ def parse():
             array.append(parsed.readline()[:-1])
 #looping for clean ',' at the end of items
         for item in array:
-          newarray.append()
-          sub_dict = {}
-          print(newarray)
-          sub_dict['user'] = item[0]
+          parts = item.split(":")
+          print(parts)
+          for part in parts:
+            print(part[2])
+"""           item_key = parts[0]
+          item_val = parts[1]
+          print[item_key] """
+"""           for part in parts:
+            sub_dict.update({part[0],part[1]})
+          #newarray.append()
+          print(sub_dict) """
           #(item[:-1])
           #print(item)
         #print(newarray)
-        json_newarray = json.dumps(newarray)
+        #json_newarray = json.dumps(newarray)
         #print(json_newarray)
       
 parse()
