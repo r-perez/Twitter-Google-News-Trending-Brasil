86b3fadf92bba9acc6afcfe42cd49fd43c813b89
Victor Góis <victorgois18@gmail.com>
2020-05-31 16:12:00 -0300

    Coleta Google News a cada 1h

--- coleta-newsapi.py
+++ coleta-newsapi.py
@@ -1,16 +1,17 @@
 from newsapi import NewsApiClient
 import time
-from datetime import datetime,timedelta
+from datetime import datetime,timedelta, date
+import json
 
-startTime = time.time()
-now = datetime.now()
-strtoday = now.strftime("%Y-%m-%d")
-stryesterday = datetime.strftime(datetime.now()-timedelta(1), '%Y-%m-%d')
+timestr = time.strftime("%Y%m%d-%H")
+
 # Init
 newsapi = NewsApiClient(api_key='2de5d157d448424db4574be570b492d4')
 
 # /v2/top-headlines
 top_headlines = newsapi.get_top_headlines(language='pt',
                                           country='br',
-                                          from_param='stryesterday',
-                                          to='strtoday')
+                                          )
+
+with open('./google-news/googlenews' + timestr + '.json','w') as f:
+    json.dump(top_headlines,f,indent=4)
