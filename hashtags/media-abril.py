32f8b8cd9acb73737b34de2f4de1a262d3d6f321
Victor Góis <victorgois18@gmail.com>
2020-06-30 21:08:17 -0300

    *

--- hashtags/media-abril.py
+++ hashtags/media-abril.py
@@ -1,7 +1,7 @@
-import json
-import csv
-import re
-import sys
+import os, json
+import pandas as pd
+from os import listdir
+from os.path import isfile, join
 
 import itertools
 from collections import Counter
@@ -11,8 +11,37 @@ json_files = [pos_json for pos_json in os.listdir('./') if pos_json.endswith('.j
 #create a list with lists of files based on the files name
 groups =  [list(g) for _, g in itertools.groupby(sorted(json_files), lambda x: x[0:8])]
 
-print(groups)
+hashtags = []
+dates = []
+hashtags_list = []
 
-with open(file, encoding='utf-8') as f:
-    json_content = json.load(f)
-    print(json_content[0][0]['trends'])
+df = pd.DataFrame(columns=['hashtags','dates'])
+
+for group in groups:
+    #for each file in the group
+    for file in group:
+        #if the file is not empty
+        if os.stat('./' + file).st_size != 0:
+            #open file for read
+            with open('./' + file, "r") as infile:
+                #load json file content and append
+                json_file = json.load(infile)                
+                ci1 = [i['name'] for i in json_file[0]['trends']]
+                hashtags.append(ci1)
+
+                                #get prefix 8 characters of files names
+                for hashtag in ci1:
+                    name = os.path.basename(infile.name)
+                    name = name[0:8]
+                    dates.append(name)
+
+for each in hashtags:
+    for hashtag in each:
+        hashtags_list.append(hashtag)
+
+        
+df = pd.DataFrame({'Hashtags':hashtags_list, 'Dates':dates})
+#df['dates'] = dates
+
+
+df.to_csv(r'./media-hashtags.csv', index=False, header=True)
