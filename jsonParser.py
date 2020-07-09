09e7fc9ebbda9f92e82f963ccb88c87e43047b61
Victor Góis <victorgois18@gmail.com>
2020-07-09 15:25:34 -0300

    *

--- jsonParser.py
+++ jsonParser.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 import numpy 
 
-with open('./perguntacorona-expanded.json', encoding='utf-8') as f:
+with open('./perguntacorona/perguntacorona-expanded.json', encoding='utf-8') as f:
     inputdata = json.load(f)
     hashtags = []
     date = []
@@ -14,6 +14,10 @@ with open('./perguntacorona-expanded.json', encoding='utf-8') as f:
     tweet_id = []
     links = []
     user_mentions = []
+    text = []
+    tweet_url = []
+    user_id = []
+    username = []
     
     i=0
     for entity in inputdata:
@@ -25,11 +29,15 @@ with open('./perguntacorona-expanded.json', encoding='utf-8') as f:
         replies.append(inputdata[i]['replies'])
         reply_to_users.append(inputdata[i]['reply_to_users'])
         retweets.append(inputdata[i]['retweets'])
-        tweet_id.append(inputdata[i]['tweet_id'])
         links.append(inputdata[i]['links'])        
+        text.append(inputdata[i]['text'])
+        text_content = text[i]
+        tweet_id.append(inputdata[i]['tweet_id'])
+        tweet_url.append(inputdata[i]['tweet_url'])
+        user_id.append(inputdata[i]['user_id'])
+        username.append(inputdata[i]['username'])
 
-        text = inputdata[i]['text']
-        user_mentions.append([x for x in text.split() if (x.startswith('@') or (x.startswith('.@')))])
+        user_mentions.append([x for x in text_content.split() if (x.startswith('@') or (x.startswith('.@')))])
 
         i += 1
     
@@ -39,11 +47,11 @@ with open('./perguntacorona-expanded.json', encoding='utf-8') as f:
                 mention.replace('.', '')
 
 
-d = {'tweet_id':tweet_id, 'hashtags':hashtags, 'date':date, 'img_urls':img_urls, 'video_url':video_url, 'replies':replies, 'reply_to_users':reply_to_users, 'retweets':retweets, 'links':links, 'user_mentions':user_mentions}
+d = {'tweet_id':tweet_id, 'user_id':user_id, 'username':username, 'text':text, 'hashtags':hashtags, 'date':date, 'img_urls':img_urls, 'video_url':video_url, 'replies':replies, 'reply_to_users':reply_to_users, 'retweets':retweets, 'links':links, 'user_mentions':user_mentions}
 df = pd.DataFrame(d)
 #df = df.explode('hashtags')
-df = df.explode('user_mentions')
-df.to_csv('perguntacorona-expanded-mentions.csv', index=False)
+#df = df.explode('user_mentions')
+df.to_csv('perguntacorona-expanded-text.csv', index=False)
 #df = pd.DataFrame(data = d, columns=['hashtags','date', 'img_urls', 'video_url', 'replies', 'reply_to_users', 'retweets'])
 
 #print(df)
